ddc1a651cd71e526fda11ff8df30e3708447610d
rohith-10-lab <rohithkumar.kumaresan@trumpf.com>
2025-12-04 13:50:20 +0530

    Review fix3

--- A04/Program.cs
+++ A04/Program.cs
@@ -5,9 +5,27 @@
 // Program.cs
 // Program to display character frequency from a word list
 // ------------------------------------------------------------------------------------------------
-string[] words = File.ReadAllLines ("C:/etc/words.txt");
-var freq = words.SelectMany (w => w)
-                .GroupBy (c => c)
-                .OrderByDescending (g => g.Count ())
-                .Take (7);
-foreach (var g in freq) Console.WriteLine ($"{g.Key} -> {g.Count ()}");
+using static System.Console;
+
+string words = File.ReadAllText ("C:/etc/words.txt");
+GetFreq2 ();
+Console.WriteLine ("--------");
+GetFreq1 ();
+
+// Computes letter frequencies using LINQ
+void GetFreq1 () {
+   var freq1 = words.Where (char.IsLetter)
+                    .GroupBy (c => c)
+                    .OrderByDescending (g => g.Count ())
+                    .Take (7);
+   foreach (var g in freq1) WriteLine ($"{g.Key} -> {g.Count ()}");
+}
+
+// Computes letter frequencies using Dictionary
+void GetFreq2 () {
+   Dictionary<char, int> freq2 = [];
+   foreach (var c in words.Where (char.IsLetter))
+      freq2[c] = freq2.TryGetValue (c, out int value) ? ++value : 1;
+   foreach (var g in freq2.OrderByDescending (x => x.Value).Take (7))
+      WriteLine ($"{g.Key} -> {g.Value}");
+}
