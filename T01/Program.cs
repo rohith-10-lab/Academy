243c4356059ec91a2dfe86e6014103038a7ebd0c
rohith-10-lab <rohithkumar.kumaresan@trumpf.com>
2025-12-29 13:07:21 +0530

    Draft commit

---
+++ T01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+var text = new StringBuilder();
+for (int i = 0; i < 100; i++) {
+   var inp = Console.ReadLine ();
+   var idx = inp.IndexOf (" ");
+   var command = inp.Substring (0, idx);
+   var s = inp.Substring (idx);
+   if (command == "ADD") {
+      text.Append (s);
+      inp = Console.ReadLine ();
+      if (inp == "SHOW") {
+         Console.WriteLine (text);
+      }
+   }
+   if (inp == "EXIT") break;
+}
+
+
