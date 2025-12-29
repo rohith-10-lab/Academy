13f3cd04f1b27b5810390c870c12ae042e37c11c
rohith-10-lab <rohithkumar.kumaresan@trumpf.com>
2025-12-29 13:39:53 +0530

    Added if else branches

--- T02/Program.cs
+++ T02/Program.cs
@@ -3,24 +3,28 @@ string[] conditions = ["L68", "L30", "R48", "L5", "R60"];
 int len = conditions.Length;
 List<int> num = [];
 for (int i = 0; i < 100; i++) num.Add (i);
-int dial = num[50],cnt = 0;
+int dial = num[50], cnt = 0, length = num.Count;
 for (int i = 0; i < len; i++) {
    string s = conditions[i];
    string k = s.Substring (1);
    bool x = int.TryParse (k, out int res);
    // for L
    if (s[0] == 'L' && dial < res) {
-      dial = num[dial + Math.Abs(num.Count - res)];
+      if (dial + res >= length) dial = num[dial + Math.Abs (length - res)];
+      else dial = num[length - (res - dial)];
    }
    else if (s[0] == 'L' && dial > res) {
-      dial = num[Math.Abs(dial - res)];
+      if (dial + res >= length) dial = num[Math.Abs (dial - res)];
+      else dial = num[dial - res];
    }
    // for R
    else if (s[0]=='R' && dial < res) {
-      dial = num[dial + res];
+     if(dial+res >= length) dial = num[dial + res - length];
+     else dial = num[dial + res];
    }
    else if (s[0] == 'R' && dial > res) {
-      dial = num[Math.Abs(dial - Math.Abs(num.Count - res))];
+      if (dial + res >= length) dial = num[dial - (num.Count - res)];
+      else dial = num[dial + res];
    }
    if(dial == 0) cnt++;
 }
